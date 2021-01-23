34945d970d4c2f5879b2761a642b170718d3c690
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2021-01-23 23:35:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def address_collector():
   with open( "addresses.txt" , 'a') as f:
     try:
       f.writelines( request.args.get('address') + "\n" )
     except:
-      return 'アドレスを取得できません'    
-  return '登録しました！' + 'NFTは本日に届きます。少々お待ちください。'
+      return 'アドレスを取得できません' 
+  if( request.args.get('address')[2] == 'x'):
+    return '登録しました！' + '動画を視聴してくれてありがとう！'
+  else:
+    return 'エラーが発生しています。もう一度お試しください'
