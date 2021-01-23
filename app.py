eeaa5fdb70497cc28dc1562d530d95a803e26b80
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2021-01-24 00:08:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def address_list():
   with open( "addresses.txt" , 'r') as f:
     try:
       add_list = str(f.readlines())
-      add_list2 = add_list.replace( "\n" , "<br>").replace("\'" , "").replace("[" , "").replace("]" , "")
+      add_list2 = add_list.replace( """\n""" , "<br>").replace("\'" , "").replace("[" , "").replace("]" , "").replace("," , "")
       return add_list2
     except:
       return 'Error!'
